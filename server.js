fa8bfa35fbefe2042e393c41581f347694f983d7
Joice-Palloma <joicepgm@gmail.com>
2020-03-05 14:15:23 +0000

    commit3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ cors = require("cors"),
 http = require("http"),
 express = require("express"),
 bodyParser = require("body-parser");
-
+mongoose = require( 'mongoose');
 var app = express();
 var port = 3000;
 
@@ -13,4 +13,12 @@ app.use(require('./routes'));
 
 app.listen(port, function(err){
     console.log("Listening on Port: " + port)
+});
+mongoose.connect('mongodb://localhost/test');
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
 });
